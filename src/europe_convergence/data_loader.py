04c3d2669d2ebaa5c8e435c56f118b705ee00e39
Juliette.Bornier <juliette.bornier.3110@gmail.com>
2024-09-05 14:16:19 +0200

    download data

--- src/europe_convergence/data_loader.py
+++ src/europe_convergence/data_loader.py
@@ -13,19 +13,90 @@ Suède
 
 def download_gdp_data():
     gdp_data = fetch_series(
-        "OECD/QNA/AUT.B1_GS1.LNBQR.Q",
-        "OECD/QNA/BEL.B1_GS1.LNBQR.Q",
-        "OECD/QNA/DEU.B1_GS1.LNBQR.Q",
-        "OECD/QNA/DNK.B1_GS1.LNBQR.Q",
-        "OECD/QNA/FRA.B1_GS1.LNBQR.Q",
-        "OECD/QNA/GRC.B1_GS1.LNBQR.Q",
-        "OECD/QNA/HUN.B1_GS1.LNBQR.Q",
-        "OECD/QNA/IRL.B1_GS1.LNBQR.Q",
-        "OECD/QNA/ITA.B1_GS1.LNBQR.Q",
-        "OECD/QNA/LTU.B1_GS1.LNBQR.Q",
-        "OECD/QNA/POL.B1_GS1.LNBQR.Q",
-        "OECD/QNA/PRT.B1_GS1.LNBQR.Q",
-        "OECD/QNA/ROU.B1_GS1.LNBQR.Q",
-        "OECD/QNA/SWE.B1_GS1.LNBQR.Q",
+        [
+            "WB/WDI/A-NY.GDP.MKTP.KD-AUS",
+            "WB/WDI/A-NY.GDP.MKTP.KD-BEL",
+            "WB/WDI/A-NY.GDP.MKTP.KD-DEU",
+            "WB/WDI/A-NY.GDP.MKTP.KD-DNK",
+            "WB/WDI/A-NY.GDP.MKTP.KD-FRA",
+            "WB/WDI/A-NY.GDP.MKTP.KD-GRC",
+            "WB/WDI/A-NY.GDP.MKTP.KD-IRL",
+            "WB/WDI/A-NY.GDP.MKTP.KD-ITA",
+            "WB/WDI/A-NY.GDP.MKTP.KD-LTU",
+            "WB/WDI/A-NY.GDP.MKTP.KD-POL",
+            "WB/WDI/A-NY.GDP.MKTP.KD-PRT",
+            "WB/WDI/A-NY.GDP.MKTP.KD-ROU",
+            "WB/WDI/A-NY.GDP.MKTP.KD-SWE",
+        ],
+        max_nb_series=20,
     )
     return gdp_data
+
+
+def download_gdp_percap():
+    gdp_percap = fetch_series(
+        [
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-AUS",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-BEL",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-DEU",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-DNK",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-FRA",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-GRC",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-IRL",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-ITA",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-LTU",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-POL",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-PRT",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-ROU",
+            "WB/WDI/A-NY.GDP.MKTP.KD.ZG-SWE",
+        ],
+        max_nb_series=20,
+    )
+    return gdp_percap
+
+
+def download_hicp_data():
+    hicp = fetch_series(
+        [
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.AT",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.BE",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.DE",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.DK",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.EL",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.ES",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.FR",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.HR",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.HU",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.IE",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.LT",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.NL",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.PL",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.PT",
+            "Eurostat/prc_hicp_fp/M.I15.CP00.FIN.SE",
+        ],
+        max_nb_series=20,
+    )
+    return hicp
+def download_lng_int_rate():
+    lng_int_rt= fetch_series(
+        [
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.AT",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.BE",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.DE",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.DK",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.EL",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.EE",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.ES",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.FI",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.FR",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.HU",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.IT",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.MT",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.NL",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.PL",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.PT",
+            "Eurostat/irt_lt_mcby_q/Q.MCBY.SE",
+        ],
+    max_nb_series= 20
+    )
+    return lng_int_rt
